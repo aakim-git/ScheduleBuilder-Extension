3a8eec84e856e196944031eceac2d657f1a18c05
agdrall <kaaron999@gmail.com>
2017-09-21 18:17:42 -0700

    improved speed with associative array

--- script.js
+++ script.js
@@ -5,41 +5,49 @@ observer.observe(target, config);
 
 
 function reload(mutations) {
-    var found_prof;
+    var found_prof = new Object();
+    found_prof.tLname = "";
+    console.log(found_prof.tLname);
     var query = document.querySelectorAll('#inlineCourseResultsContainer a[href^="mailto"]');
+    var list = [];
 
     // loop through each teacher in the result list
     for (var i = 0 ; i < query.length ; i = i + 3) {
         if (query[i].innerHTML === "TBA") { continue; }
+        var page;
         var firstName = query[i].innerHTML.substring(0, 1);
         var lastName = query[i].innerHTML.substring(3, query[i].innerHTML.length);
+        if (list[lastName] != undefined) {
+            if (list[lastName] === 1) { page = 0; }
+            else { found_prof = list[lastName]; }
+        }
 
         // do the ajax call
         if (lastName < "M") {
-            for (var page = 1 ; (found_prof == undefined || found_prof.tLname != lastName) && page < 215 ; page++) {
-                found_prof = searchPage(page, firstName, lastName);
-                /*if (found_prof.tLname > lastName) {
-                    found_prof == undefined;
-                    break;
-                }*/
+            for (page = 1 ; found_prof.tLname != lastName && page < 215 ; page++) {
+                (function (index) {
+                    found_prof = searchPage(index, firstName, lastName);
+                })(page);
             }
         }
         else {
-            for (var page = 214 ; (found_prof == undefined || found_prof.tLname != lastName) && page > 0 ; page--) {
-                found_prof = searchPage(page, firstName, lastName);
-                /*if (found_prof.tLname < lastName) {
-                    found_prof == undefined;
-                    break;
-                }*/
+            for (page = 214 ; found_prof.tLname != lastName && page > 0 ; page--) {
+                (function (index) {
+                    found_prof = searchPage(index, firstName, lastName);
+                })(page);
             }
         }
 
         // output results
-        if (found_prof == undefined) {
+        if (page === 0 || page === 215) {
+            console.log("coudnlt find " + lastName);
+            list[lastName] = 1;
             query[i].title = "Teacher is not currently in Rate My Professor ( probably new )";
             query[i].href = "http://www.ratemyprofessors.com/ShowRatings.jsp?tid=" + 0;
         }
         else {
+            console.log("found " + found_prof.tLname);
+            list[lastName] = found_prof;
             query[i].title = "Dept: " + found_prof.tDept + "\nOverall Rating: " + found_prof.overall_rating + "\nNum of Ratings: " + found_prof.tNumRatings;
             query[i].href = "http://www.ratemyprofessors.com/ShowRatings.jsp?tid=" + found_prof.tid;
         }
@@ -49,10 +57,8 @@ function reload(mutations) {
 
 
 function searchPage(page, firstName, lastName) {
-    // use hash table to improve performance
-    console.log("2 : " + lastName);
-    var found_prof;
-    //var found = false;
+    var found_prof = new Object();
+    found_prof.tLname = "";
     var j;
     var request = new XMLHttpRequest();
     request.open('GET', 'https://www.ratemyprofessors.com/filter/professor/?department=&institution=University+Of+California%2C+Davis&page=' + page + '&filter=teacherlastname_sort_s+asc&query=*%3A*&queryoption=TEACHER&queryBy=schoolId&sid=1073', false);
@@ -62,13 +68,9 @@ function searchPage(page, firstName, lastName) {
             for (var j = 0 ; j < 20 ; j++) {
                 if (data.professors[j].tLname === lastName && data.professors[j].tFname.substring(0, 1) === firstName) {
                     found_prof = data.professors[j];
-                   // found = true;
                     break;
                 }
             }
-            /*if (found == false) {
-                found_prof = data.professors[j];
-            }*/
         }
     };
     request.send();
